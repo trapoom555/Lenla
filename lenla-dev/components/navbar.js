6d136c34e023968cda070bd6e72c98ae83478979
trapoom <trapoom555@gmail.com>
2022-01-27 17:25:11 +0700

    save load diagram UI

--- lenla-dev/components/navbar.js
+++ lenla-dev/components/navbar.js
@@ -1,15 +1,80 @@
+import { useState } from 'react';
+import Popup from 'reactjs-popup';
 export default function Navbar() {
+    let diagramName = ['tp1', 'tp2', 'tp3', 'tp4'] // load from Database
+    let diagramList = []
+    const [loadItemIdx, setLoadItemIdx] = useState(0);
+
+    for(let i = 0; i < diagramName.length; i++) {
+      diagramList.push(<button className="load_diagram_items" onClick={() => setLoadItemIdx(i)}>{diagramName[i]}</button>)
+    }
+
     return (
-      <div className = "nav_bar">
-        <div className = "left_nav_bar">
-            <img className = "img_logo" />
-            <ul className = "nav_bar_list">
-                <li> File </li>
-                <li> Edit </li>
-                <li> View </li>
-                <li> Help </li>
-            </ul>
+      // <div className = "nav_bar">
+      //   <div className = "left_nav_bar">
+      //       <img className = "img_logo" />
+      //       <ul className = "nav_bar_list">
+      //           <li> File </li>
+      //           <li> Edit </li>
+      //           <li> View </li>
+      //           <li> Help </li>
+      //       </ul>
+      //   </div>
+      // </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light nav_bar">
+      <img className = "img_logo" />
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          File
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <Popup trigger={<button class="dropdown-item"> Save</button>} position="right center" modal ={true}>
+            <div className="modal_wrapper">
+              <div className="save_diagram_header">Save Diagram</div>
+              <input className="save_diagram_input" placeholder='Name'/>
+              <button className="save_diagram_button">Save</button>
+            </div>
+          </Popup>
+
+          <Popup trigger={<button class="dropdown-item"> Load</button>} position="right center" modal ={true}>
+            <div className="modal_wrapper">
+              <div className="save_diagram_header">Load Diagram</div>
+              <div className="all_diagram_wrapper">
+               {diagramList}
+              </div>
+              <button className="load_diagram_button">Load</button>
+            </div>
+          </Popup>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Trapoom</a>
         </div>
-      </div>
+      </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Edit
+            </a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              View
+            </a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Help
+            </a>
+          </li>
+          </ul>
+          </div>
+    </nav>
     )
   }
--- lenla-dev/pages/_app.js
+++ lenla-dev/pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/navbar.css";
+import 'reactjs-popup/dist/index.css';
 import "../styles/app.css";
 import "../styles/previewbutton.css";
 import "../styles/sharebutton.css";
@@ -14,9 +15,20 @@ import "../styles/insp_sum.css";
 import "../styles/toggle.css";
 import "../styles/insp_layout.css";
 import { useState } from "react";
+import Head from "next/head"
 function MyApp({ Component, pageProps }) {
     const [user, setUser] = useState({ username: "-" });
-    return <Component {...pageProps} user={user} setUser={setUser} />;
+    return (
+        <>
+            <Head>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+            </Head>
+            <Component {...pageProps} user={user} setUser={setUser} />
+        </>
+    );
 }
 
 export default MyApp;
