457ba9aacf463ae789639b2eca264227235940d2
qwerwa <32535483+qwerwa@users.noreply.github.com>
2022-02-09 01:23:06 +0700

    -api find diagram by id instead of name

--- lenla-dev/components/API.js
+++ lenla-dev/components/API.js
@@ -1,50 +1,47 @@
 const Domain = "http://localhost:3001";
 import fetch from "isomorphic-unfetch";
 import router from "next/router";
-export async function login(email,password,setUser){
-    
-        try {
-            const res = await fetch(Domain + "/log-in", {
-                // mode: "no-cors",
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email, password }),
-            });
-            //
-            if (res.status == 200) {
-                [console.log("success")];
-                router.push("/create");
-            }
-            else{
-                console.log(res.status)
-            }
-            const profile_data = await res.json();
-            setUser({ ...profile_data,password });
-            console.log("get profile done");
-            // const img_res = await fetch(Domain + "/profileImg", {
-            //     // mode: "no-cors",
-            //     method: "POST",
-            //     headers: {
-            //         Accept: "application/json",
-            //         "Content-Type": "application/json",
-            //     },
-            //     body: JSON.stringify({ email, password }),
-            //     // body: JSON.stringify({ email, password }),
-            // });
-            // const img_data = await img_res.json();
-            // setUser({ ...profile_data, profileImage: img_data.profileImg,password });
-            // console.log(user);
-            // console.log("get profileImg done");
-        } catch (error) {
-            // console.log(res);
-            console.log(error);
+export async function login(email, password, setUser) {
+    try {
+        const res = await fetch(Domain + "/log-in", {
+            // mode: "no-cors",
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        //
+        if (res.status == 200) {
+            [console.log("success")];
+            router.push("/create");
+        } else {
+            console.log(res.status);
         }
-    
+        const profile_data = await res.json();
+        setUser({ ...profile_data, password });
+        console.log("get profile done");
+        // const img_res = await fetch(Domain + "/profileImg", {
+        //     // mode: "no-cors",
+        //     method: "POST",
+        //     headers: {
+        //         Accept: "application/json",
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({ email, password }),
+        //     // body: JSON.stringify({ email, password }),
+        // });
+        // const img_data = await img_res.json();
+        // setUser({ ...profile_data, profileImage: img_data.profileImg,password });
+        // console.log(user);
+        // console.log("get profileImg done");
+    } catch (error) {
+        // console.log(res);
+        console.log(error);
+    }
 }
-export async function saveDiagram(email,password,name,elements,publicVal){
+export async function saveDiagram(email, password, name, elements, publicVal) {
     const res = await fetch(Domain + "/diagram/save/", {
         // mode: "no-cors",
         method: "POST",
@@ -52,19 +49,53 @@ export async function saveDiagram(email,password,name,elements,publicVal){
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password,name,elements,public:publicVal }),
+        body: JSON.stringify({
+            email,
+            password,
+            name,
+            elements,
+            public: publicVal,
+        }),
     });
     //
     if (res.status == 201) {
         [console.log("success")];
-    }
-    else{
-        console.log(res.status)
+    } else {
+        console.log(res.status);
     }
     // const profile_data = await res.json();
 }
-
-export async function loadDiagramName(email,password){
+export async function createDiagram(
+    email,
+    password,
+    name,
+    elements,
+    publicVal
+) {
+    const res = await fetch(Domain + "/diagram/create/", {
+        // mode: "no-cors",
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email,
+            password,
+            name,
+            elements,
+            public: publicVal,
+        }),
+    });
+    //
+    if (res.status == 201) {
+        [console.log("success")];
+    } else {
+        console.log(res.status);
+    }
+    // const profile_data = await res.json();
+}
+export async function loadDiagramName(email, password) {
     // console.log(email, password)
     const res = await fetch(Domain + "/diagram/allDiagramName/", {
         // mode: "no-cors",
@@ -73,7 +104,7 @@ export async function loadDiagramName(email,password){
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password}),
+        body: JSON.stringify({ email, password }),
         // body: JSON.stringify({
         //     name,
         //     elements:[],
@@ -84,33 +115,33 @@ export async function loadDiagramName(email,password){
     //
     if (res.status == 201) {
         [console.log("success")];
+    } else {
+        console.log(res.status);
     }
-    else{
-        console.log(res.status)
-    }
-    return await res.json()
+    return await res.json();
     // const profile_data = await res.json();
 }
-export async function loadDiagram(email,password,name){
-    const res = await fetch(Domain + "/diagram/findByName/", {
+export async function loadDiagram(email, password, id) {
+    const res = await fetch(Domain + "/diagram/findById/", {
         // mode: "no-cors",
         method: "POST",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password,name }),
+        body: JSON.stringify({ email, password, id }),
     });
     //
     if (res.status == 201) {
         [console.log("success")];
+        const data = await res.json();
+        return data;
+    } else {
+        console.log(res.status);
     }
-    else{
-        console.log(res.status)
-    }
-    console.log(res)
-    const data = await res.json();
-    console.log(data)
-    console.log("fuck2")
-    return data;
+    // console.log(res);
+    // // console.log("fuck1");
+
+    // console.log(data);
+    // console.log("fuck2");
 }
--- lenla-dev/components/navbar.js
+++ lenla-dev/components/navbar.js
@@ -1,120 +1,213 @@
-import { useState } from 'react';
-import Popup from 'reactjs-popup';
-import { loadDiagram, loadDiagramName, saveDiagram, test } from './API';
-export default function Navbar({ user, elements,setElements }) {
-  let diagramName = ["tes","fuck"];
-  let loadIndex = 0;
-  // load from Database
-  let diagramList = []
-  for (let i = 0; i < diagramName.length; i++) {
-    diagramList.push(<button className="load_diagram_items" >{diagramName[i]}</button>)
-  }
-  async function setDiagramNameList() {
-    diagramList = []
-    diagramName = await loadDiagramName(user.email, user.password)
-    console.log(diagramName)
+import { useState } from "react";
+import Popup from "reactjs-popup";
+import { loadDiagram, loadDiagramName, saveDiagram, test } from "./API";
+export default function Navbar({ user, elements, setElements }) {
+    let diagramName = ["tes", "fuck"];
+    let loadIndex = 0;
+    // load from Database
+    let diagramList = [];
     for (let i = 0; i < diagramName.length; i++) {
-      diagramList.push(<button className="load_diagram_items" onClick={
-        () => {
-          // setLoadItemIdx(i)
-          loadIndex = i;
-          console.log("done select")
-        }}>{diagramName[i]}</button>)
+        diagramList.push(
+            <button className="load_diagram_items">
+                {diagramName[i].name}
+            </button>
+        );
     }
-    console.log("load done")
-  }
-  setDiagramNameList();
-  
-  const [saveName, setSaveName] = useState('');
-  // const [loadItemIdx, setLoadItemIdx] = useState(0);
+    async function setDiagramNameList() {
+        diagramList = [];
+        diagramName = await loadDiagramName(user.email, user.password);
+        console.log(diagramName);
+        for (let i = 0; i < diagramName.length; i++) {
+            diagramList.push(
+                <button
+                    className="load_diagram_items"
+                    onClick={() => {
+                        // setLoadItemIdx(i)
+                        loadIndex = i;
+                        console.log("done select");
+                    }}
+                >
+                    {diagramName[i].name}
+                </button>
+            );
+        }
+        console.log("load done");
+    }
+    setDiagramNameList();
 
-  
+    const [saveName, setSaveName] = useState("");
+    // const [loadItemIdx, setLoadItemIdx] = useState(0);
 
-  return (
-    // <div className = "nav_bar">
-    //   <div className = "left_nav_bar">
-    //       <img className = "img_logo" />
-    //       <ul className = "nav_bar_list">
-    //           <li> File </li>
-    //           <li> Edit </li>
-    //           <li> View </li>
-    //           <li> Help </li>
-    //       </ul>
-    //   </div>
-    // </div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light nav_bar">
-      <img className="img_logo" />
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    return (
+        // <div className = "nav_bar">
+        //   <div className = "left_nav_bar">
+        //       <img className = "img_logo" />
+        //       <ul className = "nav_bar_list">
+        //           <li> File </li>
+        //           <li> Edit </li>
+        //           <li> View </li>
+        //           <li> Help </li>
+        //       </ul>
+        //   </div>
+        // </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light nav_bar">
+            <img className="img_logo" />
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              File
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <Popup trigger={<button class="dropdown-item"> Save</button>} position="right center" modal={true}>
-                <div className="modal_wrapper">
-                  <div className="save_diagram_header">Save Diagram</div>
-                  <input className="save_diagram_input" value={saveName} placeholder='Name' onChange={e => { setSaveName(e.currentTarget.value); }} />
-                  <button className="save_diagram_button" onClick={
-                    () => {
-                      console.log(user);
-                      saveDiagram(user.email, user.password, saveName, elements,true)//change public value
-                      setDiagramNameList()
-                    }
-                  }>Save</button>
-                </div>
-              </Popup>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            File
+                        </a>
+                        <div
+                            class="dropdown-menu"
+                            aria-labelledby="navbarDropdown"
+                        >
+                            <Popup
+                                trigger={
+                                    <button class="dropdown-item"> Save</button>
+                                }
+                                position="right center"
+                                modal={true}
+                            >
+                                <div className="modal_wrapper">
+                                    <div className="save_diagram_header">
+                                        Save Diagram
+                                    </div>
+                                    <input
+                                        className="save_diagram_input"
+                                        value={saveName}
+                                        placeholder="Name"
+                                        onChange={(e) => {
+                                            setSaveName(e.currentTarget.value);
+                                        }}
+                                    />
+                                    <button
+                                        className="save_diagram_button"
+                                        onClick={() => {
+                                            console.log(user);
+                                            createDiagramcreateDiagram(
+                                                user.email,
+                                                user.password,
+                                                saveName,
+                                                elements,
+                                                true
+                                            ); //change public value
+                                            setDiagramNameList();
+                                        }}
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+                            </Popup>
 
-              <Popup
-                trigger={<button class="dropdown-item"  > Load</button>} position="right center" modal={true}
-                onClick={() => {
-                  console.log("fuck")
-                  diagramList = loadDiagramName(user.email, user.password)
-                }}>
+                            <Popup
+                                trigger={
+                                    <button class="dropdown-item"> Load</button>
+                                }
+                                position="right center"
+                                modal={true}
+                                onClick={() => {
+                                    console.log("fuck");
+                                    diagramList = loadDiagramName(
+                                        user.email,
+                                        user.password
+                                    );
+                                }}
+                            >
+                                <div className="modal_wrapper">
+                                    <div className="save_diagram_header">
+                                        Load Diagram
+                                    </div>
+                                    <div className="all_diagram_wrapper">
+                                        {diagramList}
+                                    </div>
+                                    <button
+                                        className="load_diagram_button"
+                                        onClick={async () => {
+                                            // console.log()
+                                            const tmp = await loadDiagram(
+                                                user.email,
+                                                user.password,
+                                                diagramName[loadIndex].id
+                                            );
+                                            console.log(tmp);
+                                            setElements(tmp.elements);
+                                        }}
+                                    >
+                                        Load
+                                    </button>
+                                </div>
+                            </Popup>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                                Trapoom
+                            </a>
+                        </div>
+                    </li>
 
-                <div className="modal_wrapper">
-                  <div className="save_diagram_header">Load Diagram</div>
-                  <div className="all_diagram_wrapper">
-                    {diagramList}
-                  </div>
-                  <button className="load_diagram_button" onClick={
-                    async() => {
-                      // console.log()
-                      const tmp = await loadDiagram(user.email,user.password,diagramName[loadIndex])
-                      
-                      setElements(tmp.elements)
-                    }
-                  }>Load</button>
-                </div>
-              </Popup>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Trapoom</a>
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Edit
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            View
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Help
+                        </a>
+                    </li>
+                </ul>
             </div>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Edit
-            </a>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              View
-            </a>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Help
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+        </nav>
+    );
 }
